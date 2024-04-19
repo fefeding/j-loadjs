362175c12822211e298c9b4d1341801c10dfd04d
fefeding <fefeding@jintengtech.com>
2024-04-19 11:28:23 +0800

    :dog: js加载库

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,12 @@
 declare var window: Window & typeof globalThis;
 
 export type jModule = {
-    async loadJS(url: string, isModule=false): Promise<any>
+    /**
+     * 加载远程js
+     * @param url JS的地址
+     * @param isModule 是否用模块的方式加载，默认false
+     */
+    loadJS(url: string, isModule?: boolean): Promise<any>
 };
 
 export default jModule;
